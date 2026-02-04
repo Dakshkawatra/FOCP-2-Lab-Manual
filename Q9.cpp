978f4d8cacfc418e5de45258038ef35b0be83756
Dakshkawatra <kawatradaksh1@gmail.com>
2026-02-05 00:38:14 +0530

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int input ;
+    cout<<"Enter any input from the keyboard : ";
+    cin>>input;
+    if(input='a','e','i','o','u','A','E','I','O','U')
+    cout<<"You entered a vowel";
+    else if()
+    
+    
+}
